24045bba96c60601f1a2d616ec07dbcfd8e953ff
jashan7167 <jashansingh9890@gmail.com>
2024-05-22 17:24:37 +0530

    learned to make rest-api in spring boot

---
+++ SpringBoot/REST-API/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into usersdetails(id,date,name) values(1,current_date(),'jshan');
+
+insert into usersdetails(id,date,name) values(2,current_date(),'changaa');
+
+
+insert into usersdetails(id,date,name) values(3,current_date(),'danga');
+
+insert into post(id,user_id,description)
+values(10,1,'I want to learn aws');
+
+
+insert into post(id,user_id,description)
+values(11,2,'I want to learn dev-ops');
+
